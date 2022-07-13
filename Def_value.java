03c1ddde40c4bcf00424e1c55326f9f9c2f7bd0a
rajesh7200 <109009994+rajesh7200@users.noreply.github.com>
2022-07-13 17:20:28 +0530

    Add files via upload

---
+++ Def_value.java
@@ -0,0 +1,16 @@
+class Def_value
+{
+	static int a;
+	static double b;
+	static char c;
+	static String d;
+	
+	public static void main(String []args)
+	{
+		System.out.println(a);
+		System.out.println(b);
+		System.out.println(c);
+		System.out.println(d);
+	}
+	
+}
